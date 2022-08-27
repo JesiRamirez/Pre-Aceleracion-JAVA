7571d3982eed3b1f39aa622b4c8e8d63dde58cff
Jesi Ramirez <ramirezjesicamarina@gmail.com>
2022-08-27 13:48:27 -0300

    creando tablas

---
+++ src/main/java/com/alkemy/disney/disney/entity/PersonajeEntity.java
@@ -0,0 +1,45 @@
+package com.alkemy.disney.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Personaje")
+@Getter
+@Setter
+
+public class PersonajeEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imagen;
+
+    private String nombre;
+
+    private int edad;
+
+    private double peso;
+
+    private String historia;
+
+    @ManyToMany(mappedBy = "personajes", cascade = CascadeType.ALL)
+    private List<PeliculaSerieEntity> peliculas = new ArrayList<>();
+
+    //Agregar o eliminar personajes
+
+    public void addPelicula(PeliculaSerieEntity pelicula){
+        this.peliculas.add(pelicula);
+    }
+
+    public void removePelicula(PeliculaSerieEntity pelicula){
+        this.peliculas.add(pelicula);
+    }
+
+
+}
